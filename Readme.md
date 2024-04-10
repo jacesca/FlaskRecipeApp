173c507f6b5d11eb720fe44f93b5b7ff12334f37
jacesca <jacesca @gmail.com>
2024-04-10 09:35:55 -0600

    App completed

---
+++ Readme.md
@@ -0,0 +1,146 @@
+# Starting instructions
+
+## Install Flask in the prepared python environment
+> pip install flask
+> pip install flask-sqlalchemy
+> pip install Flask-Migrate
+
+## Verify the flask version
+> flask --version
+
+## Create the requirement files:
+> conda list -e >> requirements.txt
+> conda env export > flask_env.yml
+
+## Starting the application
+> python main.py
+
+## Creating tables (Tables will be created in instance folder)
+> python
+>
+> from main import db, app
+>
+> app_ctx = app.app_context()
+>
+> app_ctx.push()
+>
+> db.create_all()
+>
+> app_ctx.pop()
+>
+> exit()
+
+## To handle migrations
+1. Initialize
+> flask --app main db init
+2. Make the changes to the database
+3. Generate the migration version and document it
+> flask --app main db migrate -m "made description from recipes nullable" 
+4. Apply changes to the database
+> flask --app main db upgrade
+
+## CRUD Operations
+### C: creating & R: reading
+> python
+>
+> from main import Recipe, db, app
+>
+> app_ctx = app.app_context()
+>
+> app_ctx.push()
+>
+> Recipe.query.all()
+>
+> db.session.add(Recipe(title="Pizza 1", description="Description for pizza 1", author="Joey"))
+>
+> pizza_2 = Recipe(title="Pizza 2", description="Description for pizza 2")
+>
+> db.session.add(pizza_2)
+>
+> Recipe.query.all()[0]
+>
+> Recipe.query.all()[1]
+>
+> Recipe.query.all()[5]  # IndexError
+>
+> Recipe.query.all()[0].title
+>
+> Recipe.query.all()[0].description
+>
+> Recipe.query.first()
+>
+> all_recipes = Recipe.query.all()
+>
+> all_recipes[0]
+>
+> Recipe.query.filter_by(title="Pizza 1").all()
+>
+> Recipe.query.get(1)  # Deprecated
+>
+> db.session.get(Recipe, 1)
+>
+> db.session.get(Recipe, 2) 
+>
+> db.session.get(Recipe, 5)  # Empty result
+>
+> db.session.commit()
+>
+> app_ctx.pop()
+>
+> exit()
+
+### D: deleting
+> python
+>
+> from main import Recipe, db, app
+>
+> app_ctx = app.app_context()
+>
+> app_ctx.push()
+>
+> Recipe.query.all()
+>
+> recipe_2 = db.session.get(Recipe, 2) 
+>
+> db.session.delete(recipe_2)
+>
+> Recipe.query.all()
+>
+> db.session.commit()
+>
+> app_ctx.pop()
+>
+> exit()
+
+
+### U: updating
+> python
+>
+> from main import Recipe, db, app
+>
+> app_ctx = app.app_context()
+>
+> app_ctx.push()
+>
+> Recipe.query.all()
+>
+> recipe_1 = db.session.get(Recipe, 1) 
+>
+> recipe_1.author
+> 
+> recipe_1.author = 'Monica' 
+>
+> db.session.commit()
+>
+> Recipe.query.filter_by(author="Monica").all()
+>
+> app_ctx.pop()
+>
+> exit()
+
+## Other material
+- [Visual Studio Code Set Up for Python Projects](https://codefinity.com/blog/Visual-Studio-Code-Set-Up-for-Python-Projects)
+- [Build fast, responsive sites with Bootstrap](https://getbootstrap.com/)
+- [Get started with Bootstrap](https://getbootstrap.com/docs/5.3/getting-started/introduction/#quick-start)
+- [Bootstrap Examples](https://getbootstrap.com/docs/5.3/examples/)
+- [How do I format a date in Jinja2?](https://stackoverflow.com/questions/4830535/how-do-i-format-a-date-in-jinja2)
---
+++ main.py
@@ -0,0 +1,115 @@
+from datetime import datetime, UTC
+from flask import Flask, redirect, render_template, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipe.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Emulating the database
+all_recipes = [
+    {'title': 'Recipe No.1', 'description': 'Some ingredients for Recipe 1',
+     'author': 'Joey'},
+    {'title': 'Recipe No.2', 'description': 'Some ingredients for Recipe 2'},
+]
+
+
+# Table definition
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=True)
+    author = db.Column(db.String(50))
+    data_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.now(UTC))
+
+    def __repr__(self):
+        return f'Recipe No.{self.id}'
+
+
+@app.route('/home2/')
+def hellohome():
+    return "Hello world!"
+
+
+@app.route('/hello/<string:name>')
+def hello(name):
+    return f"Hello {name}!"
+
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    num_recipes = Recipe.query.count()
+    return render_template('index.html', num_recipes=num_recipes)
+
+
+@app.route('/recipes_dict')
+def recipes_dict():
+    return render_template('recipes_dict.html', recipes=all_recipes)
+
+
+@app.route('/list_recipes', methods=['GET', 'POST'])
+def recipes_all_together():
+    if request.method == 'POST':
+        recipe_title = request.form['title']
+        recipe_description = request.form['description']
+        recipe_author = request.form['author']
+        new_recipe = Recipe(title=recipe_title, description=recipe_description,
+                            author=recipe_author)
+        db.session.add(new_recipe)
+        db.session.commit()
+        return redirect('/list_recipes')
+    else:
+        all_recipes = Recipe.query.all()
+        return render_template('list_recipes.html', recipes=all_recipes)
+    
+
+@app.route('/recipes', methods=['GET', 'POST'])
+def recipes():
+    all_recipes = Recipe.query.all()
+    return render_template('recipes.html', recipes=all_recipes)
+
+
+@app.route('/recipes/new', methods=['GET', 'POST'])
+def new_recipe():
+    if request.method == 'POST':
+        recipe_title = request.form['title']
+        recipe_description = request.form['description']
+        recipe_author = request.form['author']
+        new_recipe = Recipe(title=recipe_title, description=recipe_description,
+                            author=recipe_author)
+        db.session.add(new_recipe)
+        db.session.commit()
+        return redirect('/recipes')
+    else:
+        return render_template('new_recipe.html')
+
+
+@app.route('/recipes/delete/<int:id>')
+def delete(id):
+    recipe = Recipe.query.get_or_404(id)
+    db.session.delete(recipe)
+    db.session.commit()
+    return redirect('/recipes')
+
+
+@app.route('/recipes/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    recipe = Recipe.query.get_or_404(id)
+    if request.method == 'POST':
+        recipe.title = request.form['title']
+        recipe.description = request.form['description']
+        recipe.author = request.form['author']
+        db.session.commit()
+        return redirect('/recipes')
+    else:
+        return render_template('edit.html', recipe=recipe)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
